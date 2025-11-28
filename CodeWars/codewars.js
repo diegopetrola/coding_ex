8d9bd6dd0d1603313956b85adbcbe0f9566ab060
diegopetrola <diegopetrola@gmail.com>
2025-11-28 16:30:00 -0300

    digital Root

--- CodeWars/codewars.js
+++ CodeWars/codewars.js
@@ -12,3 +12,10 @@ function likes(names) {
   if (n == 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
   else return `${names[0]}, ${names[1]} and ${n - 2} others like this`;
 }
+
+function digitalRoot(n) {
+  while (n > 9) {
+    n = (n % 10) + Math.floor(n / 10);
+  }
+  return n;
+}
