b34eea03b660735891f5a40057393bf884572f32
diegopetrola <diegopetrola@gmail.com>
2025-11-28 15:11:26 -0300

    Added first file for codewars exercises

---
+++ CodeWars/codewars.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-unused-vars */
+
+/**
+ * @param {[string]} names
+ */
+function likes(names) {
+  const n = names.length;
+
+  if (n == 0) return "no one likes this";
+  if (n == 1) return `${names[0]} likes this`;
+  if (n == 2) return `${names[0]} and ${names[1]} like this`;
+  if (n == 3) return `${names[0]}, ${names[1]} and ${names[2]} like this`;
+  else return `${names[0]}, ${names[1]} and ${n - 2} others like this`;
+}
